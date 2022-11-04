25773a941e6377bf766f27342d3ab474296784f3
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2022-11-03 23:13:25 -0400

    created file

---
+++ Main.py
@@ -0,0 +1,27 @@
+###############
+#Name of programmer: Areeba Minhaj
+#Date: November 2022
+#Programming principles
+#program: Perform the object-oriented analysis process to find 
+#    the objects a given a program is made of. Once the objects 
+#    are found, complete the object-oriented design process to 
+#    find the classes that correspond to the objects found. With
+#    the object-oriented design finalized, implement the program 
+#    using Visual Studio Code IDE
+##############
+
+class Calcuations:
+    def SMP(): #Simulate Monthly Production 
+        print("bleh for now")
+    def SOTP(): #sale of the product
+        print("bleh for now")
+print("Welcome to Programming Principles Sample Product Inventory")
+PC = input("Please enter the Product Code: ")
+PN = input("Please enter the Product Name: ")
+CS = input("Please enter the Current Stock: ")
+SP = input("Please enter the Product Sale Price: ")
+MC = input("Please enter the Product Manufacture Cost: ")
+ESMP = input("Please enter Estimated Monthly Production: ")
+print("******Programming Principles Sample Stock Statement******")
+
+
