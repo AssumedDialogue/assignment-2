577a3bd7ad76e976a624619cecdbabbff394aa28
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2022-11-22 22:42:52 -0500

    Milestone 1 project creations

---
+++ Banking Assignment.py
@@ -0,0 +1,12 @@
+###############
+#Name of programmer: Areeba Minhaj
+#Date: November 2022
+#Programming principles
+#program: creating and imitate a bank and bank accounts like acvings accounts and chequing accounts
+##############
+
+class interaction: #handles the interaction with the user
+    def __init__(self): #constructor method
+        print("bleh")
+    def showMainMenu(self):
+        print("bleh")
